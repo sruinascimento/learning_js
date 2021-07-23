df74802af7a974c943a34d5cfb6de668ec87a8d7
TitanGod <sruy19@gmail.com>
2021-07-23 09:30:34 -0300

    Aula sobre: true, false, null e undefined

---
+++ varLetConst.js
@@ -0,0 +1,19 @@
+const usuarioLogado = true;
+const contaPaga = false;
+
+//Truthy or falsy
+
+// 1--True, 0--False
+
+console.log(0 == false);
+console.log(''==false);
+
+console.log(`1 == true --> ${1==true}`);
+
+//null, undefined
+
+let var1 = null;
+let var2;
+
+console.log(`Tipo de dado var1 ${typeof(var1)}`);
+console.log(`Tipo de dado var2 ${typeof(var2)}`);
