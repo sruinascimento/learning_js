d1318507fc4ade33f79d945c4ced0bc0dbd3c727
TitanGod <sruy19@gmail.com>
2021-07-24 22:36:35 -0300

    aula referente ao console e error de maneira superflúa

---
+++ console.js
@@ -0,0 +1,9 @@
+//O que é o console
+
+const minhaVar = 3456
+
+console.log('Hello world');
+console.log(minhaVar);
+console.error('Something wrong is happened');
+
+console.error(new Error('deu ruim'));
---
+++ erros.js
@@ -0,0 +1,6 @@
+//erro
+//const numero -- tem que ser declarado um valor
+const numero = 0;
+//console.log(`OI`
+console.log(`oi`);
+//console.log(minhaVar) erro de referência
